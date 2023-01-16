4b3dfea31fb3f4ec8ff23a2f87ec4a3aac915486
BakhtiyarTayir <bakhtiyartayir@gmail.com>
2023-01-16 23:54:57 +0500

    Урок 19 - Аннотация @Value (Spring для Начинающих)

--- src/spring_introduction/Person.java
+++ src/spring_introduction/Person.java
@@ -1,8 +1,18 @@
 package spring_introduction;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component("personBean")
 public class Person {
     private Pet pet;
+    @Value("${person.surname}")
     private String surname;
+
+    @Value("${person.age}")
+    private int age;
 
     public String getSurname() {
         return surname;
@@ -20,13 +30,19 @@ public class Person {
         this.age = age;
     }
 
-    private int age;
 
-
-    public Person(){
+    @Autowired
+    public Person(@Qualifier("catBean") Pet pet) {
         System.out.println("Person bean is created");
+        this.pet = pet;
     }
 
+//    public Person(){
+//        System.out.println("Person bean is created");
+//    }
+
+//    @Autowired
+//    @Qualifier("dog")
     public void setPet(Pet pet) {
         System.out.println("Class Person: set pet");
         this.pet = pet;
