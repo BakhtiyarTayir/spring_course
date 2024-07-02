9453fabdfd17144581a901d9ea34ea03637d42fa
bakhtiyarTayir <bakhtiyartayir@gmail.com>
2024-07-02 12:53:27 +0500

    29. Комбинирование Pointcut

---
+++ src/aop/aspects/LoggingAndSecurityAspect.java
@@ -0,0 +1,71 @@
+package aop.aspects;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class LoggingAndSecurityAspect {
+
+    @Pointcut("execution(* aop.UniLibrary.*(..))")
+    private void allMethodsFromUniLibrary() {
+    }
+
+    @Pointcut("execution(public void aop.UniLibrary.returnMagazine())")
+    private void returnMagazineFromUniLibrary() {
+    }
+
+    @Pointcut("allMethodsFromUniLibrary() && !returnMagazineFromUniLibrary()")
+    private void allMethodsExceptReturnMagazineFromUniLibrary(){
+    }
+
+    @Before("allMethodsExceptReturnMagazineFromUniLibrary()")
+    private void beforeAllMethodsExceptReturnMagazineAdvice() {
+        System.out.println("beforeAllMethodsExceptReturnMagazineAdvice: log#10");
+    }
+
+
+//    @Pointcut("execution(* aop.UniLibrary.get*())")
+//    private void allGetMethodsFromUniLibrary() {
+//
+//    }
+//
+//    @Pointcut("execution(* aop.UniLibrary.return*())")
+//    private void allReturnMethodsFromUniLibrary() {
+//    }
+//
+//    @Pointcut("allGetMethodsFromUniLibrary() || allReturnMethodsFromUniLibrary()")
+//    private void allGetAndReturnMethodsFromUniLibrary(){}
+//    @Before("allGetMethodsFromUniLibrary()")
+//    private void beforeGetLoggingAdvice() {
+//        System.out.println("beforeGetLoggingAdvice: writing log #1");
+//    }
+//
+//    @Before("allReturnMethodsFromUniLibrary()")
+//    private void beforeReturnLoggingAdvice() {
+//        System.out.println("beforeReturnLoggingAdvice: writing log #2");
+//    }
+//
+//    @Before("allGetAndReturnMethodsFromUniLibrary()")
+//    private void beforeGetAndReturnLoggingAdvice() {
+//        System.out.println("beforeGetAndReturnLoggingAdvice: writing log #3");
+//    }
+
+
+//    @Pointcut("execution(* get*())")
+//    private void allGetMethods(){}
+//
+//    @Before("allGetMethods()")
+//    public void beforeGetLoggingAdvice() {
+//        System.out.println("beforeGetBookAdvice: логирование " +
+//                "попытки получить книгу/журнал");
+//    }
+//
+//    @Before("allGetMethods()")
+//    public void beforeGetSecurityAdvice() {
+//        System.out.println("beforeGetSecurityAdvice: проверка прав на получение" +
+//                " книги/журнала");
+//    }
+}
